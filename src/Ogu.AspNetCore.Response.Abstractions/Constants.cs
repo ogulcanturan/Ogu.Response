f0c3f1afebef3272b9f944805189023fb3ed09f3
Oğulcan TURAN <ismetogulcanturan@gmail.com>
2024-10-27 19:35:20 +0100

    removed redundant braces

--- src/Ogu.AspNetCore.Response.Abstractions/Constants.cs
+++ src/Ogu.AspNetCore.Response.Abstractions/Constants.cs
@@ -4,6 +4,6 @@ namespace Ogu.AspNetCore.Response.Abstractions
 {
     public static class Constants
     {
-        public static readonly HashSet<int> NoResponseStatusCodes = new HashSet<int>() { 204, 205, 304 };
+        public static readonly HashSet<int> NoResponseStatusCodes = new HashSet<int> { 204, 205, 304 };
     }
 }
